f3112fd00344e8ffbb87b3a1b79a9d4dab57f229
code-ex0 <louis.sasse@protonmail.com>
2025-09-13 19:40:44 +0200

    feat(kube-prometheus-stack): add Kube Prometheus Stack module with configuration and values

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -9,3 +9,6 @@ module "argo-rollouts" {
 module "nginx-ingress-controller" {
   source = "./resources/nginx-ingress-controller"
 }
+module "kube-prometheus-stack" {
+  source = "./resources/kube-prometheus-stack"
+}
