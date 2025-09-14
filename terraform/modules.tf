e07a830f5d65175cbe09c53aa9658badd5f653c7
code-ex0 <louis.sasse@protonmail.com>
2025-09-14 11:12:14 +0200

    refactor(terraform): remove Argo Rollouts and Nginx Ingress Controller modules

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,14 +1,3 @@
 module "argocd" {
   source = "./resources/argocd"
 }
-
-module "argo-rollouts" {
-  source = "./resources/argo-rollouts"
-
-}
-module "nginx-ingress-controller" {
-  source = "./resources/nginx-ingress-controller"
-}
-module "kube-prometheus-stack" {
-  source = "./resources/kube-prometheus-stack"
-}
